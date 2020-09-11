a592478f4f7a7c0cd45deeae7d481ded25227923
tonalmathew <tonalmathew1999@gmail.com>
2020-09-11 21:46:50 +0530

    env added

--- server/app.js
+++ server/app.js
@@ -5,19 +5,15 @@ const morgan = require('morgan')
 const axios = require('axios')
 require('dotenv').config()
 
-console.log(process.env)
-
 const app = express()
-
 
 app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())
 
 app.get('/status', (req, res) => {
-    const api_key = process.env.API_KEY;
-    console.log(api_key)
-    const url = `https://wakatime.com/api/v1/users/current/stats/last_7_days?api_key=${api_key}`;
+    const api_key = process.env.API_KEY
+    const url = `https://wakatime.com/api/v1/users/current/stats/last_7_days?api_key=${api_key}`
     axios.get(url)
     .then((response)=>{
         res.send(response.data)
